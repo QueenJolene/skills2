2ffca976c9fd8b2ee2cb10ff4ba0950500459cd0
Hackbright Student <info@hackbrightacademy.com>
2015-01-23 16:22:35 -0800

    Passes test for count_unique() and common_items()

--- skills2.py
+++ skills2.py
@@ -40,7 +40,7 @@ def count_unique(string1):
     For example:
 
         >>> print_dict(count_unique("each word appears once"))
-        {'appears': 1, 'each': 1, 'once': 1, 'word': 0}
+        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
   
     Words that appear more than once should be counted each time:
 
@@ -55,9 +55,18 @@ def count_unique(string1):
         {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
 
     """
+    list_of_words = string1.split()
+    word_count_dict = {}
 
-    return {}
-    
+    for item in list_of_words:
+        if item not in word_count_dict:
+            word_count_dict[item] = 1
+        else:
+            word_count_dict[item] += 1
+
+    return word_count_dict
+
+
 
 def common_items(list1, list2):
     """Produce the set of common items in two lists.
@@ -65,7 +74,7 @@ def common_items(list1, list2):
     Given two lists, return a list of the common items shared between 
     the lists.
 
-    IMPORTANT: you may not not 'if ___ in ___' or the method 'index'.
+    IMPORTANT: you may not use 'if ___ in ___' or the method 'index'.
 
 
     For example:
@@ -86,162 +95,178 @@ def common_items(list1, list2):
 
     """
 
-    return []
+    new_list = list1 + list2
+    new_dict = {}
+    final_list = []
+
+    for item in new_list:
+        if item not in new_dict:
+            new_dict[item] = 1
+        else:
+            new_dict[item] += 1
+
+    for key, value in new_dict.items():
+        if value > 1:
+            for item in range(value - 1):
+                final_list.append(key)
 
 
-def unique_common_items(list1, list2):
-    """Produce the set of *unique* common items in two lists.
-
-    Given two lists, return a list of the *unique* common items shared between 
-    the lists.
-
-    IMPORTANT: you may not not 'if ___ in ___' or the method 'index'.
+    return final_list
 
 
-    Just like `common_items`, this should find [1, 2]:
+# def unique_common_items(list1, list2):
+#     """Produce the set of *unique* common items in two lists.
 
-        >>> sorted(unique_common_items([1, 2, 3, 4], [1, 2]))
-        [1, 2]
+#     Given two lists, return a list of the *unique* common items shared between 
+#     the lists.
 
-    However, now we only want unique items, so for these lists, don't show 
-    more than 1 or 2 once:
-
-        >>> sorted(unique_common_items([1, 2, 3, 4], [1, 1, 2, 2]))
-        [1, 2]
-
-    """
-
-    return []
+#     IMPORTANT: you may not not 'if ___ in ___' or the method 'index'.
 
 
-def sum_zero(list1):
-    """Return list of x,y number pair lists from a list where x+y==0
+#     Just like `common_items`, this should find [1, 2]:
 
-    Given a list of numbers, add up each individual pair of numbers.
-    Return a list of each pair of numbers that adds up to 0.
+#         >>> sorted(unique_common_items([1, 2, 3, 4], [1, 2]))
+#         [1, 2]
+
+#     However, now we only want unique items, so for these lists, don't show 
+#     more than 1 or 2 once:
+
+#         >>> sorted(unique_common_items([1, 2, 3, 4], [1, 1, 2, 2]))
+#         [1, 2]
+
+#     """
+
+#     return []
+
+
+# def sum_zero(list1):
+#     """Return list of x,y number pair lists from a list where x+y==0
+
+#     Given a list of numbers, add up each individual pair of numbers.
+#     Return a list of each pair of numbers that adds up to 0.
 
         
-    For example:
+#     For example:
 
-        >>> sort_pairs( sum_zero([1, 2, 3, -2, -1]) )
-        [[-2, 2], [-1, 1]]
+#         >>> sort_pairs( sum_zero([1, 2, 3, -2, -1]) )
+#         [[-2, 2], [-1, 1]]
 
-    This should always be a unique list, even if there are
-    duplicates in the input list:
+#     This should always be a unique list, even if there are
+#     duplicates in the input list:
 
-        >>> sort_pairs( sum_zero([1, 2, 3, -2, -1, 1, 1]) )
-        [[-2, 2], [-1, 1]]
+#         >>> sort_pairs( sum_zero([1, 2, 3, -2, -1, 1, 1]) )
+#         [[-2, 2], [-1, 1]]
 
-    Of course, if there are one or more zeros to pair together, 
-    that's fine, too:
+#     Of course, if there are one or more zeros to pair together, 
+#     that's fine, too:
 
-        >>> sort_pairs( sum_zero([1, 2, 3, -2, -1, 1, 0, 1, 0]) )
-        [[-2, 2], [-1, 1], [0, 0]]
+#         >>> sort_pairs( sum_zero([1, 2, 3, -2, -1, 1, 0, 1, 0]) )
+#         [[-2, 2], [-1, 1], [0, 0]]
 
-    """
+#     """
 
-    return []
-
-
-def find_duplicates(words):
-    """Given a list of words, return the list with duplicates removed.
-
-    For example:
-
-        >>> sorted(find_duplicates(
-        ...     ["rose", "is", "a", "rose", "is", "a", "rose"]))
-        ['a', 'is', 'rose']
-
-    You should treat differently-capitalized words as different:
-
-        >>> sorted(find_duplicates(
-        ...     ["Rose", "is", "a", "rose", "is", "a", "rose"]))
-        ['Rose', 'a', 'is', 'rose']
-
-    """
-
-    return []
+#     return []
 
 
-def word_length(words):
-    """Given list of words, return list of ascending [(len, [words])].
+# def find_duplicates(words):
+#     """Given a list of words, return the list with duplicates removed.
 
-    Given a list of words, return a list of tuples, ordered by word-length.
-    Each tuple should have two items--the length of the words for that 
-    word-length, and the list of words of that word length.
+#     For example:
 
-    For example:
+#         >>> sorted(find_duplicates(
+#         ...     ["rose", "is", "a", "rose", "is", "a", "rose"]))
+#         ['a', 'is', 'rose']
 
-        >>> word_length(["ok", "an", "apple", "a", "day"])
-        [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
+#     You should treat differently-capitalized words as different:
 
-    """
+#         >>> sorted(find_duplicates(
+#         ...     ["Rose", "is", "a", "rose", "is", "a", "rose"]))
+#         ['Rose', 'a', 'is', 'rose']
 
-    return []
+#     """
 
-
-def adv_word_length_sorted_words(words):
-    """Given list of words, return list of ascending [(len, [sorted-words])].
-
-    Given a list of words, return a list of tuples, ordered by word-length.
-    Each tuple should have two items--the length of the words for that 
-    word-length, and the list of words of that word length. The list of words
-    for that length should be sorted alphabetically.
-
-    For example:
-
-        >>> adv_word_length_sorted_words(["ok", "an", "apple", "a", "day"])
-        [(1, ['a']), (2, ['an', 'ok']), (3, ['day']), (5, ['apple'])]
-
-    """
-
-    return []
+#     return []
 
 
-def pirate_talk(phrase):
-    """Translate phrase to pirate talk.
+# def word_length(words):
+#     """Given list of words, return list of ascending [(len, [words])].
 
-    Given a phrase, translate each word to the Pirate-speak equivalent.
-    Words that cannot be translated into Pirate-speak should pass through
-    unchanged. Return the resulting sentence.
+#     Given a list of words, return a list of tuples, ordered by word-length.
+#     Each tuple should have two items--the length of the words for that 
+#     word-length, and the list of words of that word length.
+
+#     For example:
+
+#         >>> word_length(["ok", "an", "apple", "a", "day"])
+#         [(1, ['a']), (2, ['ok', 'an']), (3, ['day']), (5, ['apple'])]
+
+#     """
+
+#     return []
+
+
+# def adv_word_length_sorted_words(words):
+#     """Given list of words, return list of ascending [(len, [sorted-words])].
+
+#     Given a list of words, return a list of tuples, ordered by word-length.
+#     Each tuple should have two items--the length of the words for that 
+#     word-length, and the list of words of that word length. The list of words
+#     for that length should be sorted alphabetically.
+
+#     For example:
+
+#         >>> adv_word_length_sorted_words(["ok", "an", "apple", "a", "day"])
+#         [(1, ['a']), (2, ['an', 'ok']), (3, ['day']), (5, ['apple'])]
+
+#     """
+
+#     return []
+
+
+# def pirate_talk(phrase):
+#     """Translate phrase to pirate talk.
+
+#     Given a phrase, translate each word to the Pirate-speak equivalent.
+#     Words that cannot be translated into Pirate-speak should pass through
+#     unchanged. Return the resulting sentence.
    
-    Here's a table of English to Pirate translations:
+#     Here's a table of English to Pirate translations:
 
-    English     Pirate
-    ----------  ----------------
-    sir         matey
-    hotel       fleabag inn
-    student     swabbie
-    boy         matey
-    madam       proud beauty
-    professor   foul blaggart
-    restaurant  galley
-    your        yer
-    excuse      arr
-    students    swabbies
-    are         be
-    lawyer      foul blaggart
-    the         th'
-    restroom    head
-    my          me
-    hello       avast
-    is          be
-    man         matey
+#     English     Pirate
+#     ----------  ----------------
+#     sir         matey
+#     hotel       fleabag inn
+#     student     swabbie
+#     boy         matey
+#     madam       proud beauty
+#     professor   foul blaggart
+#     restaurant  galley
+#     your        yer
+#     excuse      arr
+#     students    swabbies
+#     are         be
+#     lawyer      foul blaggart
+#     the         th'
+#     restroom    head
+#     my          me
+#     hello       avast
+#     is          be
+#     man         matey
 
-    For example:
+#     For example:
 
-        >>> pirate_talk("my student is not a man")
-        'me swabbie be not a matey'
+#         >>> pirate_talk("my student is not a man")
+#         'me swabbie be not a matey'
 
-    You should treat words with punctuation as if they were different
-    words:
+#     You should treat words with punctuation as if they were different
+#     words:
 
-        >>> pirate_talk("my student is not a man!")
-        'me swabbie be not a man!'
+#         >>> pirate_talk("my student is not a man!")
+#         'me swabbie be not a man!'
 
-    """
+#     """
 
-    return ""
+#     return ""
 
 
 ##############################################################################
